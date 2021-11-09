c5e51140cf804c249af26ef476b3bce1ed905d00
NahuelMonin <nahuel.monin@gmail.com>
2021-11-09 17:07:52 -0300

    Comence el test del solver

---
+++ src/arbitraje/SolverTest.java
@@ -0,0 +1,14 @@
+package arbitraje;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SolverTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
