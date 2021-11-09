77cebc947bc9ad835a8860eebdd3438f025972c3
NahuelMonin <nahuel.monin@gmail.com>
2021-11-09 17:08:31 -0300

    'reload'

--- src/arbitraje/Solver.java
+++ src/arbitraje/Solver.java
@@ -15,35 +15,42 @@ public class Solver {
 		Arbitro seleccionado = null;
 
 		for (FechaTorneo fechaActual : this.instancia.getFechasTorneo()) {
+			//Al inicio de cada fecha, todos los arbitros deben estar disponibles.
 			todosDisponibles(arbitrosDisponibles);
 
 			for (Partido partidoActual : fechaActual.getPartidos()) {
-				seleccionado = asignarArbitro(partidoActual, arbitrosDisponibles);
+				//para el partido actual, selecciona el mejor arbitro disponible
+				seleccionado = buscarCandidato(partidoActual, arbitrosDisponibles);
 				partidoActual.setArbitro(seleccionado);
 			}
 		}
 
 	}
 
-	private Arbitro asignarArbitro(Partido partido, Collection<Arbitro> arbitrosDisponibles) {
+	private Arbitro buscarCandidato(Partido partido, Collection<Arbitro> arbitrosDisponibles) {
 		Arbitro candidato = null; // arbitro a asignar
 		Integer sumaPartidos = null; // cant veces que dirigio a los equipos del partidos.
-
+		
+		//guardamos los codigos de los equipos (son las posiciones en el array de Arbitro).
 		int A = partido.getEquipoA().getCodigo();
 		int B = partido.getEquipoB().getCodigo();
 
 		for (Arbitro arbitro : arbitrosDisponibles) {
+			//Chequeamos que el arbitro este disponible para la fecha del partido.
 			if (arbitro.isDisponible()) {
+				//Sumamos la cantidad de veces que ya dirigio a los equipos del partido actual. 
 				int sumaPartidosDirigidos = arbitro.getEquiposDirigidos()[A] + arbitro.getEquiposDirigidos()[B];
 
+				//El ciclo compara con cada arbitro y guarda la mejor opcion encontrada hasta el momento.				
 				if (sumaPartidos == null || sumaPartidosDirigidos < sumaPartidos) {
 					candidato = arbitro;
 					sumaPartidos = sumaPartidosDirigidos;
 				}
 			}
 		}
+		// Una vez que termino de recorre y encontro al mejor candidato
 		// Suma al registro de equipos dirigidos del arbitro los equipos de este partido
-		// y cambia su disponibilidad para los siguientes partidos de la fecha.
+		// Y cambia su disponibilidad para los siguientes partidos de la fecha.
 		candidato.sumarEquipo(A);
 		candidato.sumarEquipo(B);
 		candidato.setDisponible(false);
@@ -56,6 +63,10 @@ public class Solver {
 		for (Arbitro arbitro : arbitrosDisponibles) {
 			arbitro.setDisponible(true);
 		}
+	}
+	
+	public Instancia getInstancia() {
+		return this.instancia;
 	}
 	
 	@Override
--- src/arbitraje/SolverTest.java
+++ src/arbitraje/SolverTest.java
@@ -2,13 +2,56 @@ package arbitraje;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.junit.Test;
 
 public class SolverTest {
-
+	
+	private Instancia crearEjemplo() {
+		
+		Equipo[] equipos = new Equipo[4];
+		equipos[0] = new Equipo("River Plate", 0);
+		equipos[1] = new Equipo("Boca Jrs", 1);
+		equipos[2] = new Equipo("Independiente", 2);
+		equipos[3] = new Equipo("Racing", 3);
+		
+		HashMap<Integer, Arbitro> arbitros = new HashMap<Integer, Arbitro>();
+		arbitros.put(1, new Arbitro(1,equipos.length));
+		arbitros.put(2, new Arbitro(2,equipos.length));
+		
+		ArrayList<Partido> partidosFecha1 = new ArrayList<Partido>();
+		partidosFecha1.add(new Partido(equipos[0], equipos[1]));
+		partidosFecha1.add(new Partido(equipos[2], equipos[3]));
+		
+		
+		ArrayList<Partido> partidosFecha2 = new ArrayList<Partido>();
+		partidosFecha2.add(new Partido(equipos[0], equipos[3]));
+		partidosFecha2.add(new Partido(equipos[2], equipos[1]));
+		
+		
+		ArrayList<Partido> partidosFecha3 = new ArrayList<Partido>();
+		partidosFecha3.add(new Partido(equipos[0], equipos[2]));
+		partidosFecha3.add(new Partido(equipos[1], equipos[3]));
+		
+		ArrayList<FechaTorneo> fechasTorneo = new ArrayList<FechaTorneo>();
+		fechasTorneo.add(new FechaTorneo (1, partidosFecha1 ));
+		fechasTorneo.add(new FechaTorneo (2, partidosFecha2 ));
+		fechasTorneo.add(new FechaTorneo (3, partidosFecha3 ));
+		
+		Instancia torneo = new Instancia(fechasTorneo, arbitros);
+		
+		return torneo;
+		
+	}
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void asignarArbitroTest() {
+		Instancia torneo = crearEjemplo();
+		Solver solucion = new Solver(torneo);
+		solucion.asignarArbitros();
+		solucion.getInstancia().g
+		
 	}
 
 }
